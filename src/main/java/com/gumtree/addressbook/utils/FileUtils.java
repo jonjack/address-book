61dd51e14115a0a0b493771e87a470d8e98bb49c
Jonathan Jackson <jonathan.x.jackson@gmail.com>
2018-06-06 11:01:25 +0100

    Fixed failing tests for FileUtils

--- src/main/java/com/gumtree/addressbook/utils/FileUtils.java
+++ src/main/java/com/gumtree/addressbook/utils/FileUtils.java
@@ -1,12 +1,31 @@
 package com.gumtree.addressbook.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
 public class FileUtils {
 
-  public static Optional<List<String>> readLinesFromSourceFile(String filename) {
-    throw new UnsupportedOperationException("Not yet implemented");
-  }
+  private static final Logger LOG = LoggerFactory.getLogger(FileUtils.class);
 
+  public static Optional<List<String>> readLinesFromSourceFile(String filename) {
+    Optional<List<String>> lines = Optional.empty();
+    Optional<URL> opt = Optional.ofNullable(FileUtils.class.getClassLoader().getResource(filename));
+    try {
+      URL url = opt.orElseThrow(IOException::new);
+      Path path = Paths.get(url.toURI());
+      lines = Optional.ofNullable(Files.readAllLines(path));
+    } catch (URISyntaxException | IOException ex) {
+      // log problem
+    }
+    return lines;
+  }
 }
