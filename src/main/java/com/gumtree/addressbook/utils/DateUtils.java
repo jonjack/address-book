7eadcae22e2fdca20198d76bb802a8f46984a145
Jonathan Jackson <jonathan.x.jackson@gmail.com>
2018-06-06 11:48:16 +0100

    Added DateUtils and some failing tests

---
+++ src/main/java/com/gumtree/addressbook/utils/DateUtils.java
@@ -0,0 +1,11 @@
+package com.gumtree.addressbook.utils;
+
+import java.time.LocalDate;
+
+public class DateUtils {
+
+  public static LocalDate stringToDateConvertor(String date) {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+}
---
+++ src/test/java/com/gumtree/addressbook/utils/DateUtilsTests.java
@@ -0,0 +1,22 @@
+package com.gumtree.addressbook.utils;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateUtilsTests {
+
+  @Test
+  public void converterReturnsExpectedDate() {
+    LocalDate date = DateUtils.stringToDateConvertor("16/03/77");
+    assertEquals(16, date.getDayOfMonth());
+    assertEquals(03, date.getMonthValue());
+    assertEquals(1977, date.getYear());
+  }
+
+}
