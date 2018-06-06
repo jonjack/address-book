b02bc7fa9b639906940a39fcd84345b2bd380a4f
Jonathan Jackson <jonathan.x.jackson@gmail.com>
2018-06-06 10:40:02 +0100

    Added a general file utility for ingesting source data

---
+++ src/main/java/com/gumtree/addressbook/utils/FileUtils.java
@@ -0,0 +1,12 @@
+package com.gumtree.addressbook.utils;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FileUtils {
+
+  public static Optional<List<String>> readLinesFromSourceFile(String filename) {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+}
